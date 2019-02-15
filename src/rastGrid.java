f6e8b7d20f49c60bfebd411b000eacf3f0aa9a82
hong <hong.niu@duke.edu>
2019-02-15 16:33:28 -0500

    second

---
+++ src/rastGrid.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class rastGrid {
+    public rastGrid(int gridSize) {
+        List[][] rasterizedGrid = new ArrayList[gridSize][gridSize];
+    }
+
+    public int getSize() {
+        return 0;
+    }
+}
+
+
+
