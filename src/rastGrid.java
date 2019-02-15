3784a08587ebb69c1e41bad01a76b5859073bfd0
hongniu99 <hongniu1024@gmail.com>
2019-02-15 17:07:40 -0500

    third

--- src/rastGrid.java
+++ src/rastGrid.java
@@ -1,14 +1,33 @@
+import javax.sound.midi.SysexMessage;
 import java.util.*;
 
 public class rastGrid {
+    private int meshSize;
+    private List[][] grid;
+
     public rastGrid(int gridSize) {
-        List[][] rasterizedGrid = new ArrayList[gridSize][gridSize];
+        meshSize = gridSize;
+        grid = new ArrayList[gridSize][gridSize];
     }
 
     public int getSize() {
-        return 0;
+        return meshSize;
+    }
+    public List[][] getGridList() {
+        return grid;
+    }
+    public void colorIn(int x, int y) {
+        grid[x][y].add(1);
+    }
+    public static void main (String[] args) {
+        rastGrid start = new rastGrid(5);
+
+        System.out.println(start.getSize());
+        System.out.println()
+        System.out.println("hi");
     }
 }
+
 
 
 
